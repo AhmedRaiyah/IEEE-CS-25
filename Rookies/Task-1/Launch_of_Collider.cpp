56d4a1dbbafc6cf3638a1ba4f0e455b647ceffe3
AhmedRaiyah <ahmed.raiyahh@gmail.com>
2024-12-02 15:47:56 +0200

    Task 1

---
+++ Rookies/Task-1/Launch_of_Collider.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string directions;
+    cin >> directions;
+    int positions[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> positions[i];
+    }
+
+    int minCollisionTime = INT_MAX;
+    
+    for (int i = 0; i < n - 1; i++) {
+        // Check for adjacent R-L pairs
+        if (directions[i] == 'R' && directions[i + 1] == 'L') {
+            // Calculate the collision time
+            int collisionTime = (positions[i + 1] - positions[i]) / 2;
+            minCollisionTime = min(minCollisionTime, collisionTime);
+        }
+    }
+
+    if (minCollisionTime == INT_MAX) {
+        cout << "-1" << endl;  // No collision occurs
+    } else {
+        cout << minCollisionTime << endl;
+    }
+
+    return 0;
+}
---
+++ Rookies/Task-1/Police_Recruits.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int police = 0, unResolved = 0; 
+
+    for(int i = 0; i < n; i++) {
+        if(i == 0 && arr[i] == -1) {
+            unResolved++;
+        } else if (i == 0 && arr[i] >= 1) {
+            police += arr[i];
+        } else {
+            if(arr[i] == -1 && police >= 1) {
+                police--;
+            } else if(arr[i] == -1 && police < 1){
+                unResolved++;
+            } else {
+                police += arr[i];
+            }
+        }
+        cout << "\nunres: " << unResolved << "\npolice: " << police << endl;
+    }
+
+    cout << "\n\n Final unresolved: " << unResolved;
+
+    return 0;
+}
